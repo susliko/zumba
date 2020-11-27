54f0ab4586fb0008a08da4912003b6fee966494d
Matvey Volkov <m.b.volkov@yandex.ru>
2020-11-27 14:46:22 +0300

    get worker by id

--- server/supervisor/routes/worker.py
+++ server/supervisor/routes/worker.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from supervisor.datas import Room, Worker, WorkerCreateBody
 from supervisor.db import RedisClient, get_db
@@ -9,14 +9,24 @@ from supervisor.utils import catch_exceptions
 worker_router = APIRouter()
 
 
-@worker_router.get('/worker/get/all/', response_model=Dict[str, Worker])
+@worker_router.get('/worker/all/', response_model=Dict[str, Worker])
 @catch_exceptions
 async def get_all_workers(db: RedisClient = Depends(get_db)):
     id_to_worker = await db.get_workers()
     return id_to_worker
 
 
-@worker_router.get('/worker/{worker_id}/rooms/', response_model=List[Room])
+@worker_router.get('/worker/id/{worker_id}/', response_model=Worker)
+@catch_exceptions
+async def get_worker_by_id(worker_id: str, db: RedisClient = Depends(get_db)):
+    id_to_worker = await db.get_workers()
+    if worker_id in id_to_worker:
+        return id_to_worker[worker_id]
+    else:
+        raise HTTPException(detail=f'Worker with id {worker_id} not found', status_code=404)
+
+
+@worker_router.get('/worker/rooms/{worker_id}/', response_model=List[Room])
 @catch_exceptions
 async def get_rooms_for_worker(worker_id: str, db: RedisClient = Depends(get_db)):
     id_to_room = await db.get_rooms()
