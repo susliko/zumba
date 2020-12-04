51668e4990e7d84e803b4b592218160f99e2bb55
Matvey Volkov <m.b.volkov@yandex.ru>
2020-12-04 13:57:46 +0300

    SpeechlessWorkerClientAsync

--- server/supervisor/worker_client.py
+++ server/supervisor/worker_client.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Any, Dict, Union
 
 from aiohttp import ClientSession, ContentTypeError
 from pydantic.main import BaseModel
@@ -18,24 +18,30 @@ class LowLevelAsyncClientMixin:
         return self._session
 
     async def _do_request(self, endpoint: str, method: str,
-                          data: Union[BaseModel, str, None] = None, timeout: int = 20):
+                          data: Union[BaseModel, str, None, Dict[str, Any]] = None,
+                          timeout: int = 20) -> None:
         data = data.dict() if isinstance(data, BaseModel) else data
         url = f'http://{self.host}:{self.port}{endpoint}'
         session = await self.get_session()
 
         async with session.request(method, url, data=data, timeout=timeout) as resp:
-            try:
-                response_dict = await resp.json()
-            except ContentTypeError:
-                response_dict = resp.content
             if resp.status >= 400:
                 raise Exception("Code {:d} returned for {} {} endpoint".format(resp.status, method, endpoint))
 
-            if response_dict:
-                return response_dict
 
-
-class WorkerClientAsync(LowLevelAsyncClientMixin):
+class SpeechlessWorkerClientAsync(LowLevelAsyncClientMixin):
 
     async def ping(self):
         await self._do_request('/ping', 'GET')
+
+    async def start_conference(self, room_id: str):
+        await self._do_request('/start_conference', 'POST', data={'conference': room_id})
+
+    async def stop_conference(self, room_id: str):
+        await self._do_request('/stop_conference', 'DELETE', data={'conference': room_id})
+
+    async def add_user(self, room_id: str, user_id: str):
+        await self._do_request('/add_user', 'POST', data={'conference': room_id, 'user': user_id})
+
+    async def remove_user(self, room_id: str, user_id: str):
+        await self._do_request('/remove_user', 'DELETE', data={'conference': room_id, 'user': user_id})
